a0d58f53c72ddc838e84a322acb69f7797bebfc4
VitoriaAmorim <vitoriajuliana118@gmail.com>
2023-02-01 15:46:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,9 @@ app.get ('/terror', function(req, res){
     res.sendFile(__dirname + '/public/HTML/terror.html');
 });
 
+app.get ('/romance', function(req, res){
+    res.sendFile(__dirname + '/public/HTML/romance.html');
+});
 
 app.listen (8080,function(){
     console.log ('Servidor rodando na porta http://localhost:8080/fantasia');
