ff38dc07630456327bb3bc639a5389cadc938e76
Antonio Carlos Ratte Gallo Filho <108150990+antoxic00@users.noreply.github.com>
2023-04-05 14:14:52 -0300

    05-04-23

--- robo.js
+++ robo.js
@@ -3,7 +3,7 @@ import axios from "axios"
 (async () => {
   let endpoint = "https://forgeware.withub.ai/api/bot/crawler/641ee870c0936eb105f684a3"
   const results = await axios.post(endpoint,
-    {inputData: {"NomeCidade":""}},
+    {inputData: {"cidade":""}},
     {
       headers: {
       "x-api-key": "91d192e2-ebe7-49a4-bfa2-29595b50971d",
